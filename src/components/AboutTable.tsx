9e2741c075a20e2a39474a0bd9c9b0d7441f2824
kwngmin <46192526+kwngmin@users.noreply.github.com>
2023-04-02 00:32:48 +0900

    Update AboutTable.tsx

--- src/components/AboutTable.tsx
+++ src/components/AboutTable.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function AboutTable({ tableData, subject }: Props) {
   return (
     <article className="mt-8">
-      <h2 className="text-center lg:text-left text-3xl font-semibold text-slate-500 py-4">
+      <h2 className="text-center lg:text-left text-3xl font-semibold text-slate-500 py-8 lg:py-4">
         {subject}
       </h2>
       <ul>
