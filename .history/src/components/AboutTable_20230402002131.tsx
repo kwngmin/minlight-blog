5b1d73197f865849a62a906594df052bf8a0cf5d
kwngmin <kwngmin.k@gmail.com>
2023-04-02 00:23:38 +0900

    스타일링 수정 및 보완

--- .history/src/components/AboutTable_20230402002131.tsx
+++ .history/src/components/AboutTable_20230402002131.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function AboutTable({ tableData, subject }: Props) {
   return (
     <article className="my-12">
-      <h2 className="text-center lg:text-left text-3xl font-semibold text-slate-500 border-b py-4">
+      <h2 className="text-center lg:text-left text-3xl font-semibold text-slate-400 border-b py-4">
         {subject}
       </h2>
       <ul>
